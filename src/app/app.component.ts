325953402b3df56a2326e4e5e76dbe888f8dd5d7
Pallavi Singh <pallavisingh992@gmail.com>
2017-10-30 00:12:51 +0530

    Intial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
+
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'ng-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Example of CRUD (Create Read Update Delete)';
+  model:any={};
+  employees = [
+    {name:"Mark", position:"CEO"},
+    {name:"Pete", position:"COO"},
+    {name:"Pallavi", position:"Developer"}
+  ];
+  addEmployee(){
+      this.employees.push(this.model);
+  }
+  deleteEmp(i){
+      console.log(i);
+  }
 }
