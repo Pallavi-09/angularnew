8828b3edbf676e3747722a12311bbac5145f5753
Pallavi Singh <pallavisingh992@gmail.com>
2018-02-13 17:26:31 +0530

    input output

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit , Input, Output, EventEmitter} from '@angular/core';
+
+import { AppComponent } from '../app.component';
 
 @Component({
   selector: 'app-about',
-  templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+  templateUrl: `<h3>{{mainStr}}</h3><button (click)="sendMsg()">Click Me</button>`
 })
 export class AboutComponent implements OnInit {
+
+
+  @Input('passStr') mainStr:string;
+
+  @Output()
+  outputVal: EventEmitter<string> = new EventEmitter<string>();
+  textMsg = "passed by child";
+  
+
+
 
   constructor() { }
 
   ngOnInit() {
+    
+    
+    
+  }
+  sendMsg(){
+    this.outputVal.emit(this.textMsg);
   }
 
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 
-import { Component } from '@angular/core';
-import { Input } from '@angular/core';
+import { Component,Input,Output,EventEmitter } from '@angular/core';
+import { AboutComponent } from './about/about.component';
 import { HttpClient} from '@angular/common/http'
 
 @Component({
@@ -9,64 +9,73 @@ import { HttpClient} from '@angular/common/http'
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'handson practice on angular';
-  model:any={};
-  name : string = '';
-  age : number;
-  found : boolean;
-  postfond : boolean;
-  postname : string = '';
-  postage : string = '';
-  birthday = new Date(1992,7,9);
-  myBirth = new Date(1992,9,7);
-  toggle = true;
-  @Input() inputString:string;
-  userStr:string='Pallavi';
-  employees = [
-    {name:"Mark", position:"CEO"},
-    {name:"Pete", position:"COO"},
-    {name:"Pallavi", position:"Developer"}
-  ];
-  namesList = ['Mark', 'Pete', 'Pallavi'];
-  addEmployee(){
-      this.employees.push(this.model);
-  }
-  deleteEmp(i){
-      console.log(i);
-  }
 
-  constructor (private httpClient:HttpClient){}
-  getName(event:any){
-      this.name = event.target.value;
-  }
-  getProfile(){
-    this.httpClient.get(`https://my-json-server.typicode.com/Pallavi-09/angularnew/profiles/?name=${this.name}`)
-    .subscribe(
-      (data:any[]) => {
-        if(data.length){
-          this.age = data[0].age;
-          this.found = true;
-        }    
-      }
-    )
-  }
-  postProfile(){
-    this.httpClient.post(`https://my-json-server.typicode.com/Pallavi-09/angularnew/profiles/`, {
-      'name':'Mom',
-      'age': 51
-    })
-    .subscribe(
-      (data:any) => {
-        this.postname = data.name;    
-        this.postage = data.age;
-        this.postfond = true;       
-      }
-    )
-  }
-  get format()   { 
-      return this.toggle ? 'shortDate' : 'fullDate'; 
-  }
-  toggleFunc() { 
-    this.toggle = !this.toggle; 
-  }
+  details : any[] = [
+    {id : "1", Name : "Pallavi", Count : 34},
+    {id : "2", Name : "Adam", Count : 8},
+    {id : "3", Name : "Nancy", Count : 12},
+    {id : "4", Name : "Pete", Count : 18},
+    {id : "5", Name : "Mark", Count : 4},
+    {id : "6", Name : "park", Count : 14},
+    {id : "7", Name : "advik", Count : 4},    
+  ]
+  // model:any={};
+  // name : string = '';
+  // age : number;
+  // found : boolean;
+  // postfond : boolean;
+  // postname : string = '';
+  // postage : string = '';
+  // birthday = new Date(1992,7,9);
+  // myBirth = new Date(1992,9,7);
+  // toggle = true;
+  // @Input() inputString:string;
+  // userStr:string='Pallavi';
+  // employees = [
+  //   {name:"Mark", position:"CEO"},
+  //   {name:"Pete", position:"COO"},
+  //   {name:"Pallavi", position:"Developer"}
+  // ];
+  // namesList = ['Mark', 'Pete', 'Pallavi'];
+  // addEmployee(){
+  //     this.employees.push(this.model);
+  // }
+  // deleteEmp(i){
+  //     console.log(i);
+  // }
+
+  // constructor (private httpClient:HttpClient){}
+  // getName(event:any){
+  //     this.name = event.target.value;
+  // }
+  // getProfile(){
+  //   this.httpClient.get(`https://my-json-server.typicode.com/Pallavi-09/angularnew/profiles/?name=${this.name}`)
+  //   .subscribe(
+  //     (data:any[]) => {
+  //       if(data.length){
+  //         this.age = data[0].age;
+  //         this.found = true;
+  //       }    
+  //     }
+  //   )
+  // }
+  // postProfile(){
+  //   this.httpClient.post(`https://my-json-server.typicode.com/Pallavi-09/angularnew/profiles/`, {
+  //     'name':'Mom',
+  //     'age': 51
+  //   })
+  //   .subscribe(
+  //     (data:any) => {
+  //       this.postname = data.name;    
+  //       this.postage = data.age;
+  //       this.postfond = true;       
+  //     }
+  //   )
+  // }
+  // get format()   { 
+  //     return this.toggle ? 'shortDate' : 'fullDate'; 
+  // }
+  // toggleFunc() { 
+  //   this.toggle = !this.toggle; 
+  // }
 }
